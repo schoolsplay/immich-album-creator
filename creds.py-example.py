53423aa7411972a5e0ebda5d4990aea38d6dbdb4
Stas Zytkiewicz <stas.zytkiewicz@gmail.com>
2025-08-24 09:42:01 +0200

    Added example creds file and gitignore file

---
+++ creds.py-example.py
@@ -0,0 +1,4 @@
+# Configuration constants (replace with your data)
+IMMICH_HOST = "<IP and port of Immich server>" # eg. 192.168.1.10:3001
+LIBRARY_ROOT = "<Absolute path of your library root folder>" # eg. /mnt/SSD/mylabrary
+API_KEY = "<Your API key>" # eg. LVonX8XvAI85EST9Ryh3fPpoUliQkSHjeRDs9Hx9I
